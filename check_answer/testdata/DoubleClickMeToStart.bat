ef8f8984e18a9a6ca5aa1a204b6543b0f87385b4
Yt <34469662+Yttrin@users.noreply.github.com>
2023-03-25 14:10:42 +0800

    Update DoubleClickMeToStart.bat

--- check_answer/testdata/DoubleClickMeToStart.bat
+++ check_answer/testdata/DoubleClickMeToStart.bat
@@ -4,11 +4,16 @@ set test=5
 
 echo You should put datainput_student_win64.exe, %jar%, and 1.in, 2.in, ... here in this folder.
 
-echo running tests
+echo ---running tests---
 
 for /l %%i in (1,1,%test%) do (copy %%i.in stdin.txt
- .\datainput_student_win64.exe | java -jar code.jar > %%i.out)
-
+echo --%%i.in--
+type %%i.in
+ .\datainput_student_win64.exe | java -jar %jar% > %%i.out
+ echo --%%i.out--
+type %%i.out
+echo --end--
+ )
 
 echo use mingw32-make.exe -f Makefile_English utest2 to use English output
 
