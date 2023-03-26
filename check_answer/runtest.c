a043c5b0b89d75ebd169aab4ed77ce45344f25e0
李政含 <21373419@buaa.edu.cn>
2023-03-26 11:14:51 +0800

    OOAutoTest v1.3

--- check_answer/runtest.c
+++ check_answer/runtest.c
@@ -4,7 +4,17 @@
 #include <direct.h>
 
 int main() {
-    int num = 1;
+    int num = 1;    
+    char folder[10] = ".\\ans";
+    if (_access(folder, 0) == -1) {
+        if (_mkdir(folder)) 
+            return 1;
+    }
+    sprintf(folder, "log.txt");
+    FILE* file = fopen(folder, "w+");
+    fprintf(file, "Test begin:\n");
+    fclose(file);
+
     printf("Please enter the number of testcases: ");
     scanf("%d", &num);
     for (int i = 1; i <= num; i++) {
@@ -17,6 +27,9 @@ int main() {
         printf("Try to generate data%d\n", i);
         sprintf(command, ".\\%s %d", data, i);
         if (system(command)) {
+            file = fopen(folder, "a+");
+            fprintf(file, "Fail to generate data%d\n", i);
+            fclose(file);
             printf("Exit program\n");
             return 0;
         }
@@ -26,17 +39,18 @@ int main() {
         printf("Running your program...\n");
         sprintf(command, "%s | java -jar %s > output.txt", input, jar_name);
         if (system(command)) {
+            file = fopen(folder, "a+");
+            fprintf(file, "Fail to run jar program\n", i);
+            fclose(file);
             printf("Exit program\n");
             return 0;
         }
         
-        char folder[10] = ".\\ans";
-        if (_access(folder, 0) == -1) {
-            if (_mkdir(folder)) 
-                return 1;
-        }
-        sprintf(command, "copy output.txt .\\ans\\ans%i.txt >> log.txt", i);
+        sprintf(command, "copy output.txt .\\ans\\ans%i.txt > NUL", i);
         if (system(command)) {
+            file = fopen(folder, "a+");
+            fprintf(file, "Fail to get output.txt\n", i);
+            fclose(file);
             printf("Exit program\n");
             return 0;
         }
@@ -45,7 +59,8 @@ int main() {
         Sleep(300);
         printf("Testing your anser:\n");
         Sleep(300);
-        system(checker);
+        sprintf(command, "%s %d", checker, i);
+        system(command);
     }
     return 0;
 }
--- check_answer/utest2.cpp
+++ check_answer/utest2.cpp
@@ -6,22 +6,27 @@ using namespace std;
 const int dataCount = 5;
 const string dic("testdata/");
 
-int main()
+int main(int argc, char* argv[])
 {
-    for (int i = 1; i <= dataCount; i++)
-    {
-        cout << "Testcase #" << i << ": ";
-        ifstream ifs(dic + to_string(i) + ".in");
-        ifstream ofs(dic + to_string(i) + ".out");
-        EventParser ep(ifs, ofs);
-        try {
-            Checker::checkAnswer(ep);
-            cout << "Accepted" << endl;
-        } catch (string& msg) {
-            cout << msg << endl;
-        }
-        ifs.close();
-        ofs.close();
+    int k = atoi(argv[1]);
+    FILE* file = fopen("log.txt", "a+");
+    fprintf(file, "#Testcase %d:\n", k);
+    ifstream ifs("stdin.txt");
+    ifstream ofs("output.txt");
+    EventParser ep(ifs, ofs);
+    try {
+        Checker::checkAnswer(ep);
+        cout << "Accepted" << endl;
+        fprintf(file, "Accepted\n");
+    } catch (string& msg) {
+        cout << msg << endl;
+        fprintf(file, "%s\n", msg.c_str());
+        fclose(file);
+        return 1;
     }
+    fclose(file);
+    ifs.close();
+    ofs.close();
+
     return 0;
 }
