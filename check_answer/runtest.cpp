b092e7bd107ca8c830fdaa4d80a71cd670e5cc47
李政含 <21373419@buaa.edu.cn>
2023-03-31 15:02:56 +0800

    run test to makeData and run Jar

--- check_answer/runtest.cpp
+++ check_answer/runtest.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "Data.h"
 #ifdef _WIN32
 #include <windows.h>
 #include <io.h>
@@ -17,11 +18,12 @@ int if_rundata();
 void read_config(const char* key, char* value);
 void __mkdir(char* filename);
 void remove_space(char* src);
+void generatedata(const char* filename, int mode = -1, int amount = 0, int flag = -1, double step = 0);
 
 int main(int argc, char* argv[]) {
     char jar_name[64];
     char input[64];
-    char command[64];
+    char command[256];
     char configbuf[64];
     char data_file[16] = {0};
     char out_file[16] = {0};
@@ -67,26 +69,28 @@ int main(int argc, char* argv[]) {
             }
         }
 
+    #ifdef _WIN32
+        sprintf(command, "copy %s stdin.txt > NUL", input);
+    #else
+        sprintf(command, "cp %s stdin.txt", input);
+    #endif
+
         printf("Running your program.\n");
         
+        
     #ifdef _WIN32
-        sprintf(command, "readata.exe %s | java -jar %s > .\\ans\\%d.out", input, jar_name, i);
+        sprintf(out_file, ".\\ans\\%d.out", i);
+        sprintf(command, "start /b runjar.exe %s %s %s", jar_name, input, out_file);
     #else
-        sprintf(command, "start readata.exe %s | java -jar %s > ./ans/%d.out", input, jar_name, i);
+        sprintf(out_file, "./ans/%d.out", i);
+        sprintf(command, "start runjar %s %s %s", input, jar_name, out_file);
     #endif
-        printf(command);
+        // printf(command);
         if (system(command)) {
             printf("RunTestErr: Fail to run program\n");
             exit(-1);
         } 
-/*
-        printf("Check your answer...\n");
-        sprintf(command, "checker.exe 0 %s output.txt", input);
-        if (system(command)) {
-            printf("Fail to check answer\n");
-            exit(-1);
-        } 
-        */
+
     }
     return 0;
 }
@@ -113,8 +117,7 @@ void rundata(const char* dataname) {
     read_config("data_step_size", configbuf);
     double data_step_size = atof(configbuf);
     sprintf(command, "rundata.exe %d %d %d %lf > %s", data_mode, data_amount, data_flag, data_step_size, dataname);
-
-    system(command);
+    generatedata(dataname, data_mode, data_amount, data_flag, data_step_size);
 }
 
 void read_config(const char* key, char* value) {
@@ -132,15 +135,16 @@ void read_config(const char* key, char* value) {
             return;
         }
     }
-    sprintf(value, "");
+    value[0] = 0;
     fclose(config);
 }
 
 void __mkdir(char* filename) {
     if (_access(filename, 0) == -1) {
-        if (_mkdir(filename)) 
+        if (_mkdir(filename)) {
             fprintf(stderr, "RunTestErr: Fail to mkdir\n");
             exit(-1);
+        }
     }
 }
 
@@ -150,4 +154,16 @@ void remove_space(char* src) {
         src[len - 1] = 0;
         len--;
     }
+}
+
+void generatedata(const char* filename, int mode, int amount, int flag, double step) { // 参数：模式、数据数量、时间步进概率、时间步进长度
+    FILE* datafile = fopen(filename, "w+");
+    if (datafile == NULL) {
+        fprintf(stderr, "GenerateDataErr: fail to open datafile\n");
+    }
+    Data new_data(mode, amount, flag, step);
+    new_data.generate();
+    fprintf(datafile, "%s", new_data.getData().c_str());
+    fclose(datafile);
+
 }
